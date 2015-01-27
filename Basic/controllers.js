697c857004f0a7ccd0d31260066e70da62d1f0b6
Mikel Torres <michel@majesticmedia.ca>
2015-01-27 19:33:38 +0100

    angular

--- Basic/controllers.js
+++ Basic/controllers.js
@@ -1,7 +1,26 @@
 var app = angular.module('app', []);
 
+// HelloWorld.html
 app.controller('HelloController', ['$scope',
   function($scope) {
     $scope.greeting = {text: 'Hello'};
+  }]
+);
+
+// Watch.html
+app.controller('WatchController', ['$scope',
+  function($scope) {
+    $scope.number = {start: 0, multiplier: 0 };
+    $scope.result = 0;
+
+    $scope.update = function() {
+      console.log($scope);
+      $scope.result = $scope.number.start * $scope.number.multiplier;
+    }
+
+    // Really ugly solution, but just wanted to check how $watch worked
+    $scope.$watch('number.start',$scope.update);
+    $scope.$watch('number.multiplier',$scope.update);
+
   }]
 );
